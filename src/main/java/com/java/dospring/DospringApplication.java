3010c1728bae71996d9b1ffaea8f6fb58fd749a8
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2025-08-27 07:14:12 +0300

    Initial commit

---
+++ src/main/java/com/java/dospring/DospringApplication.java
@@ -0,0 +1,13 @@
+package com.java.dospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/dospring/DospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
